fb19c0d2d1e5e46c46311d8b9737a38511446084
Armand Philippot <git@armandphilippot.com>
2021-06-10 16:13:16 +0200

    chore: define version, logo and colors

--- sdotit.sh
+++ sdotit.sh
@@ -2,8 +2,10 @@
 #
 # Sdotit
 # A dotfiles manager to quickly setup your machine & synchronize your dotfiles.
+#
 # Requirements: Git and GNU Stow.
 # Author: Armand Philippot <https://www.armandphilippot.com/>
+# URL: https://github.com/ArmandPhilippot/sdotit
 
 ###############################################################################
 #
@@ -30,3 +32,28 @@
 # SOFTWARE.
 #
 ###############################################################################
+
+set -e
+
+SDOTIT_VERSION="0.1.0"
+SDOTIT_LOGO=$(
+  cat <<-EOF
+################################################################
+##                 _____                                      ##
+##                / ____|    _       _   _ _                  ##
+##               | (___   __| | ___ | |_(_) |_                ##
+##                \___ \ / _  |/ _ \| __| | __|               ##
+##                ____) | (_| | (_) | |_| | |_                ##
+##               |_____/ \__,_|\___/ \__|_|\__|               ##
+##                                                            ##
+################################################################
+EOF
+)
+
+
+_error_color=$'\e[31m'
+_success_color=$'\e[32m'
+_warning_color=$'\e[33m'
+_choice_color=$'\e[34m'
+_output_color=$'\e[35m'
+_no_color=$'\e[0m'
